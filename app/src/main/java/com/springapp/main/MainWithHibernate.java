261c1c8f884f4ec7f1670182501c5cb93666c9b9
alex livenson <alexlivenson@alexs-mbp.companynet.org>
2015-01-21 17:40:22 -0800

    some clean up

--- app/src/main/java/com/springapp/main/MainWithHibernate.java
+++ app/src/main/java/com/springapp/main/MainWithHibernate.java
@@ -1,12 +1,7 @@
 package com.springapp.main;
 
-import com.springapp.mvc.dao.IEmployeeDAO;
-import com.springapp.mvc.model.Employee;
-import com.springapp.mvc.service.EmployeeService;
 import com.springapp.mvc.service.IEmployeeService;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -15,14 +10,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MainWithHibernate {
 
     public static void main(String[] args) {
-        //ApplicationContext context = new AnnotationConfigApplicationContext(IEmployeeDAO.class);
         ApplicationContext context = new ClassPathXmlApplicationContext("META-INF/spring/applicationContext.xml");
-//        EmployeeService service = context.getBean(EmployeeService.class);
         IEmployeeService service = context.getBean(IEmployeeService.class);
-//
-//
-//        service.findAllEmployees();
-//        System.out.println(service.findAllEmployees());
 
         service.findAllEmployees();
         System.out.println(service.findAllEmployees());
